817c02d68658de92782d0cc5d2a56aa7e46a4b61
AiShuka <faxasun@gmail.com>
2025-01-15 17:59:58 +0900

    バリデーションの修正
    ヴ、ヵ、ヶを含めるため

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
   validates_confirmation_of :password
 
   # 名前のバリデーション
-  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
 
-  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ンー]+\z/ }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
 
    # 生年月日のバリデーション
   validates :birth_date, presence: true
