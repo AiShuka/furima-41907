d78a50bf2bb8379ac8fccb50676f49ffc2ee57d7
AiShuka <faxasun@gmail.com>
2025-01-30 14:22:12 +0900

    購入機能の実装

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,6 +1,6 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :shipping_from_id, :city, :street_address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :shipping_from_id, :city, :street_address, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :item_id
@@ -10,10 +10,19 @@ class PurchaseForm
     validates :city
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+    validates :token
   end
 
-  def save(params,user_id)
-    purchase = Purchase.create(item_id: params[:item_id].to_i, user_id: user_id)
-    Address.create(postal_code: postal_code, shipping_from_id: shipping_from_id, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  def save
+    purchase = Purchase.create(user_id:, item_id:)
+    Address.create(
+      postal_code:,
+      shipping_from_id:,
+      city:,
+      street_address:,
+      building_name:,
+      phone_number:,
+      purchase_id: purchase.id
+    )
   end
 end
