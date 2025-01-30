1182d2d27c01ed7fe2c7d5f3ba2f34017e2b0e39
AiShuka <faxasun@gmail.com>
2025-01-30 16:35:29 +0900

    自身の商品は購入不可

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :redirect_if_seller, only: [:index, :create]
 
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
@@ -25,6 +26,12 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def redirect_if_seller
+    if current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
@@ -39,6 +46,5 @@ class PurchasesController < ApplicationController
       :postal_code, :shipping_from_id, :city, :street_address, :building_name, :phone_number)
       .merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token]
       )
-    end
   end
 end
